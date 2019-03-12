c323c91876a05ae32bafdd6923eabe78ecab8328
Logen Gendron <logeng@uw.edu>
2019-03-11 22:32:34 -0700

    updates to bike data

--- bikeincidents.R
+++ bikeincidents.R
@@ -4,6 +4,8 @@ library(httr)
 library(jsonlite)
 library(ggmap)
 library(ggplot2)
+library(leaflet)
+library(htmltools)
 
 # Bike incidents 
 
@@ -18,4 +20,36 @@ bikes_df <- as.data.frame(parsed_data)
 response_locations <- GET("https://bikewise.org:443/api/v2/locations?proximity=Seattle&proximity_square=100")
 body_locations <- content(response_locations, "text")
 parsed_data_locations <- fromJSON(body_locations)
-bikes_df_locations <- as.data.frame(parsed_data_locations)
+bikes_df_locations <- parsed_data_locations$features$geometry
+
+
+# Get seperate lat/long coordinates
+
+bikes_df_locations <- separate(bikes_df_locations, col = coordinates, 
+                               into = c("long", "lat"), sep = ",")
+bikes_df_locations$long <- gsub("c\\(", "", bikes_df_locations$long)
+bikes_df_locations$lat <- gsub("\\)", "", bikes_df_locations$lat)
+
+# Make the columns numeric 
+
+bikes_df_locations$lat <- as.numeric(bikes_df_locations$lat)
+bikes_df_locations$long <- as.numeric(bikes_df_locations$long)
+
+# Join the data frames
+
+bikes <- cbind.data.frame(bikes_df, bikes_df_locations)
+
+# Leaflet plot 
+
+
+incidents_map <-
+  leaflet(data = bikes) %>%
+  addTiles() %>%
+  addCircles(
+    lng = ~long,
+    lat = ~lat,
+    label = ~htmlEscape(incidents.title),
+    stroke = T
+  )
+
+
